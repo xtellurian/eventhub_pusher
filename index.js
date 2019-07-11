40eb16bdb71af9a0f6499f6502bde5a06744e4af
Rian Finnegan <rian.finnegan@gmail.com>
2019-07-11 14:08:47 +1000

    add code

---
+++ index.js
@@ -0,0 +1,30 @@
+const { EventHubClient } = require("@azure/event-hubs");
+const moment = require("moment");
+
+// Define connection string and the name of the Event Hub
+const connectionString = process.env.CONNECTION_STRING;
+const eventHubsName = process.env.EVENT_HUB_NAME;
+
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+
+async function main() {
+  const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName);
+
+  while (true) {
+    let body = {_t: moment()}
+    const eventData = {body: JSON.stringify(body)};
+    console.log(`Sending message: ${eventData.body}`);
+    await client.send(eventData);
+    await sleep(5000);
+  }
+
+  await client.close();
+}
+
+main().catch(err => {
+  console.log("Error occurred: ", err);
+});
