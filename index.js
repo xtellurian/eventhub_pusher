c619035b5e01ba57acad7f07e664f91198f18013
Rian Finnegan <rian.finnegan@gmail.com>
2019-07-12 18:13:09 +1000

    sine sender

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ const { EventHubClient } = require("@azure/event-hubs");
 const moment = require("moment");
 
 // Define connection string and the name of the Event Hub
-// const connectionString = process.env.CONNECTION_STRING;
-// const eventHubsName = process.env.EVENT_HUB_NAME;
+const connectionString = process.env.CONNECTION_STRING;
+const eventHubsName = process.env.EVENT_HUB_NAME;
 
-const connectionString="Endpoint=sb://demeter-test.servicebus.windows.net/;SharedAccessKeyName=writer;SharedAccessKey=cr6ktAnEl/pdj80Pt1Lb9SnbM5MCot9uEXyY5KolUR4=;EntityPath=demeter";
-
-const eventHubsName = "demeter";
-
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
-    })
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
 
 async function main() {
-  const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName);
+  const client = EventHubClient.createFromConnectionString(
+    connectionString,
+    eventHubsName
+  );
 
   while (true) {
-    let body = {t: moment(), s: "hello world!"}
-    const eventData = {body: JSON.stringify(body)};
-    console.log(`Sending message: ${eventData.body}`);
+    let h = moment().hour() / 24;
+    const body = {
+      t: moment()
+        .utc()
+        .format("YYYY-MM-dTHH:mm:ss"),
+      sine: Math.sin(h),
+      s: "sine"
+    };
+    const eventData = { body: body };
+    console.log(`Sending message: ${JSON.stringify(eventData)}`);
     await client.send(eventData);
     await sleep(5000);
   }
